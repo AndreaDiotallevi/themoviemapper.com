a0867326a2c1459d6401242a6f4fdd2ff624539b
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-04-09 14:20:18 +0100

    handle map click

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,35 @@
 import React from "react"
+import axios from "axios"
+import { navigate } from "gatsby"
 import GoogleMapReact from "google-map-react"
 
 const IndexPage = () => {
+    const handleClick = async ({
+        lat,
+        lng,
+    }: GoogleMapReact.ClickEventValue) => {
+        try {
+            const response = await axios.get(
+                `https://us1.locationiq.com/v1/reverse.php?key=${process.env.REACT_APP_LOCATION_API}&lat=${lat}&lon=${lng}&format=json`
+            )
+
+            if (!response) return
+
+            const countryCode = response.data.address.country_code
+            if (!countryCode) return
+
+            const regionNamesInEnglish = new Intl.DisplayNames(["en"], {
+                type: "region",
+            })
+            const country = regionNamesInEnglish.of(countryCode.toUpperCase())
+            if (!country) return
+
+            navigate(`/${country.replace(/\s+/g, "-").toLowerCase()}`)
+        } catch (err) {
+            return
+        }
+    }
+
     return (
         <div style={{ height: "100vh", width: "100%" }}>
             <GoogleMapReact
@@ -11,7 +39,7 @@ const IndexPage = () => {
                     lng: 0,
                 }}
                 defaultZoom={2.4}
-                onClick={value => console.log(value)}
+                onClick={handleClick}
             />
         </div>
     )
