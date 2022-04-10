a073a3914b456e6df4a93c84e21e058097f3fff7
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-04-10 11:31:17 +0100

    update countries json

--- src/countries/create_json.py
+++ src/countries/create_json.py
@@ -2,15 +2,23 @@ import json
 
 newList = []
 
-with open('movies.json', 'r+') as f:
+with open('all_countries.json', 'r+') as f:
     data = json.load(f)
     for country in list(data):
-        newList.append({
-            "country": country,
-            "movies": data[country]
-        })
+        newList.append(country)
+
 
     print(newList)
 
-with open("newJson.json", "w") as outfile:
+with open('movies.json', 'r+') as f:
+    data = json.load(f)
+
+    for country in newList:
+        try:
+            country["movies"] = data[country["name"]]
+        except:
+            country["movies"] = []
+        
+
+with open("new.json", "w") as outfile:
     json.dump(newList, outfile, indent=4)
