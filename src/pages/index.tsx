9f20abeb586d84a35bc423847e2f69cf7a2b2347
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-04-10 11:05:03 +0100

    amend map styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -99,9 +99,6 @@ const IndexPage = () => {
                                 {
                                     color: "#FFFFFF",
                                 },
-                                // {
-                                //     weight: 0.6,
-                                // },
                             ],
                         },
                         {
