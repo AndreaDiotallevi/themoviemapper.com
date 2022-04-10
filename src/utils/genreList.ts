7665ec704e07841b30603d06fbc59bcd839528d2
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-04-10 11:04:18 +0100

    convert to typescript

--- src/utils/genreList.ts
+++ src/utils/genreList.ts
@@ -1,4 +1,14 @@
-export default [
+export type Genre =
+    | "Action"
+    | "Comedy"
+    | "Crime"
+    | "Drama"
+    | "Romance"
+    | "Sci-Fi"
+    | "Thriller"
+    | "All"
+
+export const genreList: Genre[] = [
     "Action",
     "Comedy",
     "Crime",
